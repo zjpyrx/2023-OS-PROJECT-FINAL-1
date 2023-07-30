4c3ef2bbd87bc1054f96170bb77cee1ad053ee49
zjpyrx <xxhtly@163.com>
2023-07-31 00:15:15 +0800

    lab8 update1

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,13 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU]; //lab8更改，拆分freelist
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++)
+    initlock(&kmem[i].lock, "kmem");  //lab8更改
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +57,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  /********lab8新增，获取 cpuid 的时候关闭中断**********************/
+  push_off(); //关闭中断，防止竞争
+  int id = cpuid();  //获取当前CPU的ID
+  pop_off(); //开启中断
+  /*****************************************************************/
+
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +77,27 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  int id = cpuid();
+  pop_off();
+
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
+  if (r) {
+    kmem[id].freelist = r->next;
+  }
+  else {  //如果当前CPU的空闲内存块链表为空，则从其他CPU的空闲内存块链表中寻找可用内存块
+    for (int i = 0; i < NCPU; i++) {
+      if (i == id) continue;
+      acquire(&kmem[i].lock);
+      r = kmem[i].freelist;
+      if (r)  //找到可用块
+        kmem[i].freelist = r->next; //更新链表头指针
+      release(&kmem[i].lock);
+      if (r) break;
+    }
+  }
+  release(&kmem[id].lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
